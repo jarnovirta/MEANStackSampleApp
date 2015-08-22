a00df1a16b38419260b04e92ce842bdfc15d6a73
jarnovirta <jarvirta1@gmail.com>
2015-08-23 01:02:19 +0300

    Add gulp task for building assets

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,4 @@ gulp.task('watch:css', function() {
 	gulp.watch('css/**/*.styl', ['css'])
 });
 gulp.task('dev', ['watch:css', 'watch:js', 'dev:server', 'start:redis-server']);
+gulp.task('build', ['js', 'css']);
